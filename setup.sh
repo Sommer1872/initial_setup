6ceee0d1c553d62166940bb5b6b8863147f8d91b
Sommer1872 <simonwsommer@gmail.com>
2018-05-14 18:49:08 +0200

    adjusted setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # requires python
 
 sudo apt-get update
@@ -9,9 +10,9 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # get setup files from my private github repository
 # this contains files to 
-git clone https://github.com/Sommer1872/setup.git
+git clone https://github.com/Sommer1872/initial_setup.git
 
-# move things to home directory and hide
+# move things to home directory and rename (hidden) 
 cd setup
 mv bashrc_example ~/.bashrc
 mv vimrc ~/.vimrc
