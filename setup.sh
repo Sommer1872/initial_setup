319ad12d3686666e0c8e1f6ecafb4483be908c40
Sommer1872 <simonwsommer@gmail.com>
2018-06-25 16:14:24 +0200

    changed setup.sh, removed cloning

--- setup.sh
+++ setup.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 # requires python
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git
-
 # get vundle (a package manager for vim)
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# get setup files from my private github repository
-# this contains files to 
-git clone https://github.com/Sommer1872/initial_setup.git
 
 # move things to home directory and rename (hidden) 
 cd setup
