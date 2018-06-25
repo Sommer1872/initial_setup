a8f14958f74a8f6fe4d2ac1aa582a96df84249e1
Sommer1872 <simonwsommer@gmail.com>
2018-06-25 16:29:30 +0200

    better setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # requires python
 
-# get vundle (a package manager for vim)
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo apt-get update
+sudo apt-get upgrade
 
 # move things to home directory and rename (hidden) 
 cd setup
 mv bashrc_example ~/.bashrc
 mv vimrc ~/.vimrc
+
+# (remove the existing vim folder)
+rm -rf ~/.vim
+
 mv vim ~/.vim
 mv git-completion.bash ~/.git-completion.bash
 mv git-prompt.sh ~/.git-prompt.sh
 cd
+
+# get vundle (a package manager for vim)
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # get and install cmake (needed for YouCompleteMe)
 sudo apt-get install cmake
