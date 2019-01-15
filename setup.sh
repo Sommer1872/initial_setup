83f918c5e8507b0ff518ea9f83dc6993a7a0037f
Simon <simonwsommer@gmail.com>
2019-01-15 07:21:33 +0100

    excluded YCM

--- setup.sh
+++ setup.sh
@@ -26,18 +26,18 @@ cd
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # get and install cmake (needed for YouCompleteMe)
-sudo apt-get install build-essential cmake
+#sudo apt-get install build-essential cmake
 
 # make sure we have python headers installed
-sudo apt-get install python-dev python3-dev
+#sudo apt-get install python-dev python3-dev
 
 # install YouCompleteMe (autocompletion for python in vim)
-cd
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-cd .vim/bundle/YouCompleteMe
-./install.py
-cd
+#cd
+#git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+#git submodule update --init --recursive
+#cd .vim/bundle/YouCompleteMe
+#./install.py
+#cd
 
 # install other vundle plugins
 vim +PluginInstall +qall
