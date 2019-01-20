e3871a0e6f1652d75861a8c9d611731bdeb56676
Simon Sommer <simonwsommer@gmail.com>
2019-01-20 14:58:03 +0100

    installing miniconda after changing bashrc

--- setup.sh
+++ setup.sh
@@ -4,11 +4,6 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-# get and install miniconda
-#wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-#bash Miniconda3-latest-Linux-x86_64.sh
-#rm Miniconda3-latest-Linux-x86_64.sh
-
 # move things to home directory and rename (hidden) 
 cd setup
 mv bashrc_example ~/.bashrc
@@ -24,6 +19,12 @@ cd
 
 # get vundle (a package manager for vim)
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# get and install miniconda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+
 
 # get and install cmake (needed for YouCompleteMe)
 #sudo apt-get install build-essential cmake
